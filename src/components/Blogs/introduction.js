ab46ece18e0283518af74094cdc0964214ece175
DeSean Prentice <desean.devo@gmail.com>
2022-08-04 14:27:10 -0400

    Fixed blog link issue

--- src/components/Blogs/introduction.js
+++ src/components/Blogs/introduction.js
@@ -1,23 +1,29 @@
-import pic5 from "../../assets/5.png";
-import Link from "@mui/material/Link";
+import pic5 from '../../assets/5.png'
+import Link from '@mui/material/Link'
 
 const introduction = {
   title: `Intoduction to LEMV`,
   id: `intro-lemv`,
   image: pic5,
-  alt: "hands hold a rainbow colored array of vegetables",
+  alt: 'hands hold a rainbow colored array of vegetables',
   body: [
-    `Livermore! Eat. More. Vegetables. (LEMV) was founded in 2022 by local
-Livermore residents looking to give back to our community and positively
-impact the health and wellbeing of everyone living in our city. We are
-currently planning a Needs Assessment to gather community feedback about
-Livermorians’ specific needs and current barriers to vegetable
-consumption.`,
-`To learn more about the people who make this possible, check out our “Who
-are we?” page on our main website: ${<a href="/who" target="_blank">www.lemv.org/who</a>}`
-    
+    <p>
+      Livermore! Eat. More. Vegetables. (LEMV) was founded in 2022 by local
+      Livermore residents looking to give back to our community and positively
+      impact the health and wellbeing of everyone living in our city. We are
+      currently planning a Needs Assessment to gather community feedback about
+      Livermorians’ specific needs and current barriers to vegetable
+      consumption.
+    </p>,
+    <p>
+      To learn more about the people who make this possible, check out our “Who
+      are we?” page on our main website:{' '}
+      <Link href='/who' target='_blank'>
+        www.lemv.org/who
+      </Link>
+    </p>
   ],
   tags: [`introduction`, `who`, `mission`, `needs assessment`, `livermore`]
-};
+}
 
-export default introduction;
+export default introduction
